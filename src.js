1e3133818ed6d9226a0d2de6c70e3313a54b4f1a
Asmita Porwal <asmita.porwal04@gmail.com>
2023-07-18 20:41:15 +0530

    Functionality of copy button

--- src.js
+++ src.js
@@ -1,5 +1,10 @@
 var bgBody = document.getElementById('main');
 
+function copyCode() {
+    let copyText = document.getElementById('code').textContent;
+    navigator.clipboard.writeText(copyText);
+    alert('Copied the text: ' + copyText);
+}
 //hex color code generator function
 function hexColorCodeGenerator() {
     let symbols = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
